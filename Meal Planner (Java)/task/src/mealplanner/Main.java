58de3c4b9c0d22bae543780c576628be5c623b1c
cagrisayir <omercagris@gmail.com>
2024-03-12 20:25:28 +0300

    Stage 2 done

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -1,30 +1,82 @@
 package mealplanner;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Meals meal = mealChecker();
+        ArrayList<Meals> allMeals = new ArrayList<>();
+        while (true) {
+            System.out.println("What would you like to do (add, show, exit)?");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                System.out.println("Bye!");
+                break;
+            } else if (input.equals("add")) {
+                Meals meal = mealChecker();
+                mealAdder(meal);
+                allMeals.add(meal);
+                System.out.println("The meal has been added!");
+            } else if (input.equals("show")) {
+                showAllMeals(allMeals);
+            }
+        }
+    }
 
-        System.out.println("Input the meal's name:");
-        meal.setName(scanner.nextLine());
+    public static void showAllMeals(ArrayList<Meals> meals) {
+        if (meals.isEmpty())
+            System.out.println("No meals saved. Add a meal first.");
+        for (var meal : meals) {
+            printMeal(meal);
+        }
+    }
 
+    public static void mealAdder(Meals meal) {
+        meal.setName(takeMealName());
+        meal.setIngredients(ingredientsAdder());
+    }
+
+    public static String[] ingredientsAdder() {
         System.out.println("Input the ingredients:");
-        String ingres = scanner.nextLine();
-        String[] ingresArray = ingres.split(",");
-        meal.setIngredients(ingresArray);
+        String[] ingresArray;
+        while (true) {
+            String ingres = scanner.nextLine();
+            ingresArray = ingres.split(",");
+            ArrayList<Integer> checkArray = new ArrayList<>();
+            for (var ing : ingresArray) {
+                if (checkFormat(ing.trim()))
+                    checkArray.add(1);
+            }
+            if (checkArray.isEmpty()) break;
+            System.out.println("Wrong format. Use letters only!");
+        }
+        return ingresArray;
+    }
 
-        printMeal(meal);
+    public static boolean checkFormat(String str) {
+        return !str.matches("^[a-zA-Z ]*$") || str.trim().isEmpty();
+    }
+
+    public static String takeMealName() {
+        System.out.println("Input the meal's name:");
+        String mealName;
+        while (true) {
+            mealName = scanner.nextLine();
+            if (!checkFormat(mealName))
+                break;
+            System.out.println("Wrong format. Use letters only!");
+        }
+        return mealName;
     }
 
     public static Meals mealChecker() {
         Meals meal;
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
-        String mealStr = scanner.nextLine();
 
         while (true) {
+            String mealStr = scanner.nextLine();
             if (mealStr.equalsIgnoreCase("breakfast")) {
                 meal = new Breakfast();
                 break;
@@ -48,6 +100,5 @@ public class Main {
         for (var ing : meal.getIngredients()) {
             System.out.println(ing.trim());
         }
-        System.out.println("The meal has been added!");
     }
 }
